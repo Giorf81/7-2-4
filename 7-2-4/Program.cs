fd3e35aac6c2bd2ebe9b7d6d2ef167ab0701ad45
Giorf81 <rejroberts7@gmail.com>
2024-11-09 14:47:36 +0300

    Добавьте файлы проекта.

---
+++ 7-2-4/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _7_2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class BaseClass
+    {
+        public virtual int Counter
+        {
+            get;
+            set;
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        private int num;
+        public override int Counter
+        { 
+            get { return num; }
+            set
+            {
+                if (num < 0)
+                    Console.WriteLine("Что то не так");
+                else 
+                 num = value;
+            }
+        }
+    }
+}
